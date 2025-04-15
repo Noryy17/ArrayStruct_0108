02e8529dc8a4425dc4ad85e55879e0439b863ec4
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-04-15 19:51:12 +0700

    menyelesaikan struct

--- struct.cpp
+++ struct.cpp
@@ -5,3 +5,26 @@ struct mahasiswa{
     string nim;
     string nama;
     string alamat;
+};
+
+int main (){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140001";
+    mhs.nama = "Abra";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat";
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "data mahasiswa" << endl;
+
+    cout << "NIM =" << mhs.nim << endl;
+    cout << "nama =" << mhs.nama << endl;
+    cout << "alamat =" << mhs.alamat << endl;
+}
